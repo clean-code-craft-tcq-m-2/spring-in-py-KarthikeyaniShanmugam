3fc172b2d5d1873df9124c1c179b9d14eb6f08a6
Karthikeyani Shanmuga Sundaram <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-10-15 13:32:52 +0530

    update1

--- statistics.py
+++ statistics.py
@@ -1,11 +1,34 @@
 from cmath import nan
 
+# def calculateStats(numbers):
+#   output = {}
+#   if len(numbers):
+#     output["avg"] = sum(numbers)/len(numbers)
+#     output["min"] = min(numbers)
+#     output["max"] = max(numbers)   
+#   else:    
+#     output["avg"] = output["min"] = output["max"] = nan
+
+#   return output
+
 def calculateStats(numbers):
   output = {}
   if len(numbers):
-    output["avg"] = sum(numbers)/len(numbers)
-    output["min"] = min(numbers)
-    output["max"] = max(numbers)   
+    minNum = float('-inf')
+    maxNum = float('inf')
+    sumNum = 0
+    for num in numbers:
+      sumNum = sumNum + num
+      if num < minNum:
+        minNum = num
+      if num > maxNum:
+        maxNum = num
+    avgNum = sumNum/len(numbers)
+    
+    
+    output["avg"] = avgNum
+    output["min"] = minNum
+    output["max"] = maxNum  
   else:    
     output["avg"] = output["min"] = output["max"] = nan
 
