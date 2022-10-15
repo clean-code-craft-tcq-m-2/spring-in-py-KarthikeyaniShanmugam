9312762e2dbd6ac8ef1d5e09fa002bbd7f439f62
Karthikeyani Shanmuga Sundaram <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-10-15 16:25:13 +0530

    Updated code for email and led alerts

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
+
 from cmath import nan
 
 def calculateStats(numbers):
@@ -11,25 +12,17 @@ def calculateStats(numbers):
 
   return output
 
-# def calculateStats(numbers):
-#   output = {}
-#   if len(numbers):
-#     minNum = float('-inf')
-#     maxNum = float('inf')
-#     sumNum = 0
-#     for num in numbers:
-#       sumNum = sumNum + num
-#       if num < minNum:
-#         minNum = num
-#       if num > maxNum:
-#         maxNum = num
-#     avgNum = sumNum/len(numbers)
-    
-    
-#     output["avg"] = avgNum
-#     output["min"] = minNum
-#     output["max"] = maxNum  
-#   else:    
-#     output["avg"] = output["min"] = output["max"] = nan
 
-  return output
+class EmailAlert():
+  emailSent = False
+class LEDAlert():
+  ledGlows = False
+class StatsAlerter():
+  def __init__(self,maxThreshold, setList):
+      self.maxThreshold = maxThreshold
+      self.setList = setList
+  def checkAndAlert(self,values):
+    computedStats = calculateStats(values)
+    if computedStats["max"] > self.maxThreshold :
+      self.setList[0].emailSent = True
+      self.setList[1].ledGlows = True
